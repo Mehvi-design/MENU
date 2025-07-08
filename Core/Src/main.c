a6d71a1347e65aa66c4cead05379c48679f39026
Mehvi <mehvishgully@gmail.com>
2025-07-08 17:19:46 +0500

    first commit

---
+++ Core/Src/main.c
@@ -0,0 +1,546 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include"string.h"
+#include "ST7735.h"
+#include"GFX_FUNCTIONS.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef hlpuart1;
+
+SPI_HandleTypeDef hspi1;
+DMA_HandleTypeDef hdma_spi1_rx;
+DMA_HandleTypeDef hdma_spi1_tx;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_LPUART1_UART_Init(void);
+static void MX_SPI1_Init(void);
+/* USER CODE BEGIN PFP */
+#define NUM_OPTIONS 4
+
+const char *menu[] = {
+	"TEST LCD",
+    "LED ON",
+    "LED OFF",
+    "LED TOGGLE",
+};
+
+
+// Draw a simple light bulb ON
+void draw_bulb_on(void) {
+	fillScreen(BLACK);
+    fillCircle(64, 40, 20, YELLOW);         // Bulb head
+    ST7735_FillRectangle(60, 60, 8, 15, GRAY);          // Bulb base
+    drawLine(64, 75, 64, 100, ORANGE);      // Glow line
+}
+void draw_bulb_off(void) {
+    fillScreen(BLACK);
+    fillCircle(64, 40, 20, GRAY);         // Bulb head
+    ST7735_FillRectangle(60, 60, 8, 15, WHITE);          // Bulb base
+    drawLine(64, 75, 64, 100, ORANGE);      // Glow line
+}
+
+void process_command(char *cmd)
+{
+
+    if (strstr(cmd, "LED ON"))
+    {
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET); // LED ON
+        char msg[] = "LED turned ON\r\n";
+        //HAL_UART_Transmit(&hlpuart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+        fillScreen(BLACK);
+        draw_bulb_on();
+        ST7735_WriteString(5, 90,msg, Font_11x18, YELLOW, BLACK);
+
+    }
+    else if (strstr(cmd, "LED OFF"))
+    {
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET); // LED OFF
+        char msg[] = "LED turned OFF\r\n";
+        //HAL_UART_Transmit(&hlpuart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+        fillScreen(BLACK);
+        draw_bulb_off();
+        ST7735_WriteString(5, 90,msg, Font_11x18,CYAN, BLACK);
+    }
+    else if (strstr(cmd, "LED TOGGLE"))
+    {
+        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
+        char msg[] = "LED Toggled\r\n";
+        //HAL_UART_Transmit(&hlpuart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+        fillScreen(BLACK);
+
+        ST7735_WriteString(5, 90,msg, Font_11x18, GREEN, BLACK);
+    }
+    else if (strstr(cmd, "TEST LCD"))
+        {
+    	   testAll();
+        }
+    else
+    {
+        char msg[] = "Invalid/Command";
+        //HAL_UART_Transmit(&hlpuart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+        fillScreen(BLACK);
+        ST7735_WriteStringWithSeparators(10, 90, msg, Font_11x18, RED, BLACK);
+    }
+}
+void draw_menu(int selected) {
+	fillScreen(BLACK);
+
+    for (int i = 0; i < NUM_OPTIONS; i++) {
+        uint16_t y = 20 + i * 20;
+        if (i == selected) {
+            ST7735_WriteString(10, y, "->", Font_11x18,YELLOW, BLACK);
+            ST7735_WriteString(30, y, (char*)menu[i], Font_11x18, YELLOW,BLACK);
+
+        } else {
+            ST7735_WriteString(10, y, (char*)menu[i], Font_11x18, WHITE,BLACK);
+        }
+    }
+}
+
+volatile uint8_t rx_ready = 0;
+char last_char;
+uint8_t rx_char;
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    if (huart->Instance == LPUART1) {
+        last_char = rx_char;
+        rx_ready = 1; // Signal main loop
+        HAL_UART_Receive_IT(&hlpuart1, &rx_char, 1);
+    }
+}
+
+
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+//char data[]="Type (LED ON)/(LED OFF)/TOGGLE\r\n";
+
+//char intro[] = "TYPE\nLED ON\nLED OFF\nTOGGLED";
+
+int selected = 0;
+
+
+
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_LPUART1_UART_Init();
+  MX_SPI1_Init();
+  /* USER CODE BEGIN 2 */
+  ST7735_Init(0);
+  fillScreen(BLACK);
+  ST7735_SetRotation (1);
+  draw_menu(selected);
+//  char rx_buffer[8];
+  HAL_UART_Receive_IT(&hlpuart1, &rx_char, 1);
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+
+	  if (rx_ready) {
+	         rx_ready = 0;
+
+	         switch (last_char) {
+	             case 'w':
+	                 selected--;
+	                 if (selected < 0) selected = NUM_OPTIONS - 1;
+	                 draw_menu(selected);
+	                 break;
+
+	             case 's':
+	                 selected++;
+	                 if (selected >= NUM_OPTIONS) selected = 0;
+	                 draw_menu(selected);
+	                 break;
+
+	             case '\r':
+	            	 fillScreen(BLACK);
+	                 process_command((char*)menu[selected]);
+	                 HAL_Delay(1000);
+	                 draw_menu(selected);
+
+	                 break;
+	         }
+	     }
+  }
+	  //	  HAL_UART_Receive(&hlpuart1, (uint8_t*)&rx_char, 1, HAL_MAX_DELAY);
+//	  HAL_UART_Transmit(&hlpuart1,(uint8_t*) &rx_char, 1, HAL_MAX_DELAY);  // Echo back
+//
+//	            switch (rx_char) {
+//	                        case 'w': // Up
+//	                            selected--;
+//	                            if (selected < 0) selected = NUM_OPTIONS - 1;
+//	                            draw_menu(selected);
+//	                            break;
+//
+//	                        case 's': // Down
+//	                            selected++;
+//	                            if (selected >= NUM_OPTIONS) selected = 0;
+//	                            draw_menu(selected);
+//	                            break;
+//
+//	                        case '\r': // Enter
+//	                      	  fillScreen(
+//	                      			  BLACK);
+//	                            process_command((char*)menu[selected]);
+//	                            HAL_Delay(2000); // Pause for 2s
+//	                            draw_menu(selected);  // Return to menu
+//
+//	                            break;
+//	               }
+
+
+//      memset(rx_buffer, 0, sizeof(rx_buffer));
+//      HAL_UART_Transmit(&hlpuart1, (uint8_t*)"\r\n", 2, HAL_MAX_DELAY);
+//      HAL_UART_Transmit(&hlpuart1,(uint8_t*)data, strlen(data), HAL_MAX_DELAY);
+     // fillCircle(5, 32, 5, BLUE);
+     // ST7735_WriteStringWithSeparators(20, 0, intro, Font_11x18, WHITE, BLACK);
+//      uint8_t idx = 0;
+//       uint8_t ch;
+
+
+
+//          while (1)
+//          {
+//              // Receive one character
+//              HAL_UART_Receive(&hlpuart1, &ch, 1, HAL_MAX_DELAY);
+//
+//              // Echo the character back
+//              HAL_UART_Transmit(&hlpuart1, &ch, 1, HAL_MAX_DELAY);
+//
+//              // Store into buffer
+//              rx_buffer[idx++] = ch;
+//
+//              // Break on newline or carriage return
+//              if (ch == '\r' || ch == '\n' || idx >= sizeof(rx_buffer) - 1)
+//              {
+//                  rx_buffer[idx] = '\0'; // Null-terminate the string
+//                  break;
+//              }
+//          }
+
+          // send a newline after the echo
+         // HAL_UART_Transmit(&hlpuart1, (uint8_t*)"\r\n", 2, HAL_MAX_DELAY);
+  //          process_command((char*)rx_buffer);
+
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief LPUART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_LPUART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN LPUART1_Init 0 */
+
+  /* USER CODE END LPUART1_Init 0 */
+
+  /* USER CODE BEGIN LPUART1_Init 1 */
+
+  /* USER CODE END LPUART1_Init 1 */
+  hlpuart1.Instance = LPUART1;
+  hlpuart1.Init.BaudRate = 115200;
+  hlpuart1.Init.WordLength = UART_WORDLENGTH_8B;
+  hlpuart1.Init.StopBits = UART_STOPBITS_1;
+  hlpuart1.Init.Parity = UART_PARITY_NONE;
+  hlpuart1.Init.Mode = UART_MODE_TX_RX;
+  hlpuart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  hlpuart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  hlpuart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&hlpuart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN LPUART1_Init 2 */
+
+  /* USER CODE END LPUART1_Init 2 */
+
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 7;
+  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi1.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel2_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel2_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel2_IRQn);
+  /* DMA1_Channel3_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel3_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel3_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+  HAL_PWREx_EnableVddIO2();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(DC_GPIO_Port, DC_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, CS_Pin|reset_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : DC_Pin */
+  GPIO_InitStruct.Pin = DC_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(DC_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : CS_Pin reset_Pin */
+  GPIO_InitStruct.Pin = CS_Pin|reset_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
